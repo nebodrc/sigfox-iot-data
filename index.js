d56e610ac7bd372b87cea0a0e612c81b58ded6a1
nebodrc <31569098+nebodrc@users.noreply.github.com>
2018-05-28 20:10:36 -0500

    Update index.js
    agregado campos ctr y vlt

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ const sensorfields = (tbl) => ({
   localdatetime: [tbl.string, false, 'Human-readable local datetime, e.g. 2017-10-12 08:59:29'],
 
   alt: [tbl.float, false, 'Altitude in metres above sea level, used by send-alt-structured demo, e.g. 86.4'],
+  vlt: [tbl.float, false, 'Volt, used by send-alt-structured demo, e.g. 86.4'],
+  ctr: [tbl.float, false, 'CTR, used by send-alt-structured demo, e.g. 86.4'],
   avgSnr: [tbl.float, false, 'Sigfox average signal-to-noise ratio, e.g. 59.84'],
   baseStationLat: [tbl.float, false, 'Sigfox basestation latitude.  Usually truncated to 0 decimal points, e.g. 1'],
   baseStationLng: [tbl.float, false, 'Sigfox basestation longitude.  Usually truncated to 0 decimal points, e.g. 104'],
@@ -297,7 +299,7 @@ exports.main = process.env.FUNCTION_NAME ? require('sigfox-gcloud/main').getMain
     const afterExec = error => error ? callback(error, 'AutoInstall Failed')
       : require('/tmp/autoinstall').installAndRunWrapper(event, context, callback, package_json, __filename, wrapper, wrap);
     if (require('fs').existsSync('/tmp/autoinstall.js')) return afterExec(null);  //  Already downloaded.
-    const cmd = 'curl -s -S -o /tmp/autoinstall.js https://raw.githubusercontent.com/UnaBiz/sigfox-iot-cloud/master/autoinstall.js';
+    const cmd = 'curl -s -S -o /tmp/autoinstall.js https://raw.githubusercontent.com/nebodrc/sigfox-iot-cloud/master/autoinstall.js';
     const child = require('child_process').exec(cmd, { maxBuffer: 1024 * 500 }, afterExec);
     child.stdout.on('data', console.log); child.stderr.on('data', console.error); return null; };
 //  exports.main is the startup function for AWS Lambda and Google Cloud Function.
